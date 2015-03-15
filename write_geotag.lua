3ecc5cd1630545ab314602d9573051bae80f13a2
arru <arru@users.noreply.github.com>
2015-03-15 18:19:28 +0100

    Report progress using dt_lua_backgroundjob_t

--- write_geotag.lua
+++ write_geotag.lua
@@ -6,13 +6,18 @@ local function isnan(x) return x ~= x end
 local function write_geotag()
   local images_to_write = {}
   local image_table = dt.gui.selection();
+  local image_table_count = 0
   
   for _,image in pairs(image_table) do
     if (not isnan(image.longitude) and not isnan(image.latitude)) then      
       table.insert(images_to_write,image)
+      image_table_count = image_table_count + 1
     end
   end
   
+  save_job = dt.gui.create_job ("Saving exif geotags", true)
+  local image_done_count = 0
+
   for _,image in pairs(images_to_write) do
     local imagePath = "'"..image.path.."/"..image.filename.."'"
     local exifCommand = "exiftool -exif:gpslatitude="..image.latitude.." -exif:gpslongitude="..image.longitude.." "..imagePath
@@ -24,8 +29,12 @@ local function write_geotag()
     coroutine.yield("RUN_COMMAND", exifCommand)
     
     dt.print("Wrote geotag for "..image.filename)
+    save_job.percent = image_table_count/image_done_count
+    image_done_count = image_done_count + 1
     
   end
+  
+  save_job.valid = false
 end
 
 dt.register_event("shortcut",write_geotag, "Write geotag to image file")
