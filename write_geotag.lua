eef3c825e9ea4cb34b0e6fb948477431fda58b35
arru <arru@users.noreply.github.com>
2015-03-17 23:24:05 +0100

    Preference to check if lat/lon is already present in file EXIF and skip writing

--- write_geotag.lua
+++ write_geotag.lua
@@ -10,17 +10,32 @@ local function write_geotag()
   local image_table = dt.gui.selection();
   local image_table_count = 0
   
+  save_job = dt.gui.create_job ("Saving exif geotags", true)
+  
   for _,image in pairs(image_table) do
     if (not isnan(image.longitude) and not isnan(image.latitude)) then
+      local includeImage = true
+      if (not dt.preferences.read("write_geotag","OverwriteGeotag","bool")) then
+        local exifReadProcess = io.popen("exiftool -n -GPSLongitude -GPSLatitude "..getImagePath(image))
+        local exifLine = exifReadProcess:read()
+        while exifLine do
+          if (exifLine ~= '') then
+            local gpsTag, gpsValue = string.match(exifLine, "(GPS %a+)%s+: ([%d%.]+)")
+            includeImage = false
+          end
+          exifLine = exifReadProcess:read()
+        end
+        exifReadProcess:close()
+      end
       
       
-      
-      table.insert(images_to_write,image)
-      image_table_count = image_table_count + 1
+      if includeImage then
+        table.insert(images_to_write,image)
+        image_table_count = image_table_count + 1
+      end
     end
   end
   
-  save_job = dt.gui.create_job ("Saving exif geotags", true)
   local image_done_count = 0
   
   for _,image in pairs(images_to_write) do
@@ -41,9 +56,6 @@ local function write_geotag()
     --Will fail and exit if image file does not exist (or path is invalid)
     coroutine.yield("RUN_COMMAND", testIsFileCommand)
     
-    
-    
-    
     coroutine.yield("RUN_COMMAND", exifCommand)
     
     dt.print("Wrote geotag for "..image.filename)
@@ -56,6 +68,7 @@ local function write_geotag()
   save_job.valid = false
 end
 
+dt.preferences.register("write_geotag", "OverwriteGeotag", "bool", "Write geotag: allow overwriting existing file geotag", "Replace existing geotag in file. If unchecked, files with lat & lon data will be silently skipped.", false )
 dt.preferences.register("write_geotag", "DeleteOriginal", "bool", "Write geotag: delete original image file", "Delete original image file after updating EXIF. When off, keep it in the same folder, appending _original to its name", false )
 dt.preferences.register("write_geotag", "KeepFileDate", "bool", "Write geotag: carry over original image file's creation & modification date", "Sets same creation & modification date as original file when writing EXIF. When off, time and date will be that at time of writing new file, to reflect that it was altered. Camera EXIF date and time code are never altered, regardless of this setting.", true )
 
