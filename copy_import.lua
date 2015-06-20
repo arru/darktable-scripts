5b2319a1d98e98f9238fbaef4e696ca1b1178bea
arru <arru@users.noreply.github.com>
2015-06-20 20:55:00 +0200

    Alternate destination directories
    Support for alternate destination directories (no UI)

--- copy_import.lua
+++ copy_import.lua
@@ -12,6 +12,9 @@ local supported_image_formats_init = {"3FR", "ARW", "BAY", "BMQ", "CAP", "CINE",
 -------- Configuration --------
 
 local mount_root = "/Volumes"
+local alternate_inbox_name = "Inbox"
+local alternate_dests = {
+}
 
 local supported_image_formats = {}
 for index,ext in pairs(supported_image_formats_init) do
@@ -57,6 +60,8 @@ end
 local import_transaction = {
   type = nil,
   srcPath = nil,
+  destRoot = nil,
+  destStructure = nil,
   destPath = nil,
   date = nil,
   tags = nil,
@@ -65,9 +70,10 @@ local import_transaction = {
 
 import_transaction.__index = import_transaction
 
-function import_transaction.new(path)
+function import_transaction.new(path, destRoot)
   local self = setmetatable({}, import_transaction)
   self.srcPath = path
+  self.destRoot = destRoot
   return self
 end
 
@@ -75,7 +81,7 @@ function import_transaction.load(self)
   --check if supported image file or movie, set self.type to 'image' or
   --'movie' or 'none' otherwise
   assert(self.srcPath ~=nil)
-  
+  assert(self.destRoot ~= nil)
   local dir, name, ext = split_path(self.srcPath)
   
   
@@ -90,11 +96,15 @@ function import_transaction.load(self)
     end
     local date = {}    
     date['year'], date['month'], date['day'], date['hour'], date['minute'], date['seconds']
-    = self.tags['Time Original']:match(exif_date_pattern)
+      = self.tags['Time Original']:match(exif_date_pattern)
     self.date = date
     
+    local dirStructure = _copy_import_dir_structure_string
+    if (self.destStructure ~= nil) then
+      dirStructure = self.destStructure
+    end
     self.type = 'image'
-    self.destPath = interp(_copy_import_dest_root.."/".._copy_import_dir_structure_string.."/"..name, self.date)
+    self.destPath = interp(self.destRoot.."/"..dirStructure.."/"..name, self.date)
   end
 end
 
@@ -117,13 +127,16 @@ function import_transaction.copy_image(self)
   assert(fileExists ~= fileNotExists)
   
   if (fileExists == nil) then
-    local copyCommand = "cp -n '"..self.srcPath.."' '"..self.destPath.."'"
+    local copyMoveCommand = "cp -n '"..self.srcPath.."' '"..self.destPath.."'"
+    if (on_same_volume(self.srcPath,self.destPath)) then
+      copyMoveCommand = "mv -n '"..self.srcPath.."' '"..self.destPath.."'"
+    end
     
     --print (makeDirCommand)
     coroutine.yield("RUN_COMMAND", makeDirCommand)
     
     --print (copyCommand)
-    coroutine.yield("RUN_COMMAND", copyCommand)
+    coroutine.yield("RUN_COMMAND", copyMoveCommand)
   else
     destDir = nil
   end
@@ -133,10 +146,16 @@ function import_transaction.copy_image(self)
   return destDir
 end
 
-local function scrape_files(root, list)
+
+-------- Subroutines --------
+
+local function scrape_files(scrapeRoot, destRoot, structure, list)
   local numFilesFound = 0
-  for imagePath in io.popen("ls "..root.."/*.*"):lines() do
-    local trans = import_transaction.new(imagePath)
+  for imagePath in io.popen("ls "..scrapeRoot.."/*.*"):lines() do
+    local trans = import_transaction.new(imagePath, destRoot)
+    --Preference value will be used if nil
+    trans.destStructure = structure
+    
     table.insert(list, trans)
     numFilesFound = numFilesFound + 1
   end
@@ -152,26 +171,40 @@ function copy_import()
   local statsNumFilesFound = 0
   local statsNumFilesProcessed = 0
   
-  _copy_import_dest_root = dt.preferences.read("copy_import","MainImportDirectory","directory")
+  local dcimDestRoot = dt.preferences.read("copy_import","MainImportDirectory","directory")
   _copy_import_dir_structure_string = dt.preferences.read("copy_import","FolderPattern","directory")
   
-  local testDestRootMounted = "test -d '".._copy_import_dest_root.."'"
+  transactions = {}
+  changedDirs = {}
+  
+  local testDestRootMounted = "test -d '"..dcimDestRoot.."'"
   local destMounted = os.execute(testDestRootMounted)
   
   if (destMounted == true) then
     statsNumFilesFound = statsNumFilesFound +
-      scrape_files(escape_path(mount_root).."/*/DCIM/*", transactions)
+      scrape_files(escape_path(mount_root).."/*/DCIM/*", dcimDestRoot, nil, transactions)
+  else
+    dt.print(dcimDestRoot.." is not mounted. Will only import from inboxes.")
+  end
+  
+    
+  for _, altConf in pairs(alternate_dests) do
+    local dir = altConf[1]
+    local dirStructure = altConf[2]
+    
+    local testAltDirExists = "test -d '"..dir.."'"
+    local altDirExists = os.execute(testAltDirExists)
+    if (altDirExists == true) then
+      local ensureInboxExistsCommand = "mkdir -p '"..dir.."/"..alternate_inbox_name.."'"
+      coroutine.yield("RUN_COMMAND", ensureInboxExistsCommand)
+      
+      statsNumFilesFound = statsNumFilesFound +
+        scrape_files(escape_path(dir).."/"..escape_path(alternate_inbox_name), dir, dirStructure, transactions)
+    else
+      dt.print(dir.." could not be found and was skipped over.")
+    end
   end
 
-  transactions = {}
-  changedDirs = {}
-  
-  for imagePath in io.popen("ls "..escape_path(mount_root).."/*/DCIM/*/*.*"):lines() do
-    local trans = import_transaction.new(imagePath)
-    table.insert(transactions,trans)
-    statsNumFilesFound = statsNumFilesFound + 1
-  end
-  
   local copy_progress_job = dt.gui.create_job ("Copying images", true)
     
   for _,tr in pairs(transactions) do
@@ -214,5 +247,5 @@ function copy_import()
 end
 
 dt.preferences.register("copy_import", "MainImportDirectory", "directory", "Copy import: root directory to import to (photo library)", "help goes here", "/" )
-dt.preferences.register("copy_import", "FolderPattern", "string", "Copy import: directory naming pattern for imports", "help goes here", "${year}/${month}/${day}" )
+dt.preferences.register("copy_import", "FolderPattern", "string", "Copy import: directory naming structure for imports", "help goes here", "${year}/${month}/${day}" )
 dt.register_event("shortcut",copy_import, "Copy and import images from DCF volumes (such as flash memory cards)")
