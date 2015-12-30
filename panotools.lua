f261a56d443b9dc64b331d4e9cb342b314fa0e87
arru <arru@users.noreply.github.com>
2015-12-30 17:47:38 +0100

    Refactored .pto creation function in preparation for HDR option

--- panotools.lua
+++ panotools.lua
@@ -16,7 +16,33 @@ end
 
 local function getImagePath(i) return "'"..i.path.."/"..i.filename.."'" end
 
-local function _create_pto()
+local function _create_project(image_table, create_command, tag)
+  local previous_image = nil
+  for _,image in pairs(image_table) do
+    create_command = create_command.." "..getImagePath(image)
+    dt.tags.attach(tag, image)
+    
+    if previous_image ~= nil then
+      image.group_with(image, previous_image)
+    end
+    previous_image = image
+  end
+
+  local create_success = os.execute(create_command)
+  assert(create_success == true)
+end
+
+local function _post_create_actions(output_path)
+  local reveal_command = "open -R "
+  reveal_command = reveal_command.." '"..output_path.."'"
+  coroutine.yield("RUN_COMMAND", reveal_command)
+
+  local hugin_command = hugin_install_path.."hugin "
+  hugin_command = hugin_command.." '"..output_path.."' &"
+  coroutine.yield("RUN_COMMAND", hugin_command)
+end
+
+local function _create_pto(mode)
   local image_table = dt.gui.selection()
   
   local num_images = 0
@@ -30,39 +56,26 @@ local function _create_pto()
   
   if num_images > 1 then
     local pto_final_path = dt.preferences.read("panotools","PTOOutputDirectory","directory")
+    
     local pto_name = first_image.filename
     if num_images <= mini_threshold then
       pto_name = pto_name.." M"
     else
-      pto_name = pto_name.." P"
+      pto_name = pto_name..mode
     end
     
     pto_temp_path = "/tmp/"..pto_name..".pto"
     pto_final_path = pto_final_path.."/"..pto_name..".pto"
     
-    local ptogen_command = hugin_install_path.."pto_gen".." -o '"..pto_temp_path.."'"
+    local create_command = hugin_install_path.."pto_gen".." -o '"..pto_temp_path.."'"
     
-    local previous_image = nil
-    for _,image in pairs(image_table) do
-      ptogen_command = ptogen_command.." "..getImagePath(image)
-      dt.tags.attach(panorama_source_tag, image)
-      
-      if previous_image ~= nil then
-        image.group_with(image, previous_image)
-      end
-      previous_image = image
-    end
-    
-    local create_success = os.execute(ptogen_command)
-    assert(create_success == true)
-    
+    _create_project(image_table, create_command, panorama_source_tag)
+
     local points_command = hugin_install_path..points_tool_pano.." -o '"..pto_final_path.."' '"..pto_temp_path.."'"
     --debug_print(points_command)
     coroutine.yield("RUN_COMMAND", points_command)
 
-    local hugin_command = hugin_install_path.."hugin "
-    hugin_command = hugin_command.." '"..pto_final_path.."'"
-    coroutine.yield("RUN_COMMAND", hugin_command)
+    _post_create_actions(pto_final_path)
   else
     dt.print("Please select at least 2 images to create panorama project")
   end
@@ -71,10 +84,10 @@ end
 function create_pto_handler()
   if (_debug) then
     --Do a regular call, which will output complete error traceback to console
-    _create_pto()
+    _create_pto('P')
   else
     
-    local main_success, main_error = pcall(_create_pto)
+    local main_success, main_error = pcall(_create_pto,'P')
     if (not main_success) then
       local error_message = "An error prevented create .pto script from completing"
       --Do two print calls, in case tostring conversion fails, user will still see a message
